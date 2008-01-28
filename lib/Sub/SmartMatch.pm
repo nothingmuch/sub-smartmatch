5ee25fdf4b7c744231e7b03884517faf783d2715
Yuval Kogman <nothingmuch@woobling.org>
2008-01-28 19:43:50 +0000

    Initial version

---
+++ lib/Sub/SmartMatch.pm
@@ -0,0 +1,235 @@
+#!/usr/bin/perl
+
+package Sub::SmartMatch;
+
+use strict;
+use warnings;
+
+use 5.010;
+
+use Carp qw(croak);
+use Scalar::Util qw(reftype);
+
+our $VERSION = "0.01";
+
+use base qw(Exporter);
+
+our @EXPORT = our @EXPORT_OK = qw(multi multi_default def_multi);
+
+
+
+BEGIN {
+	# If we have Sub::Name, great. If not, nevermoose
+
+	local $@;
+
+	eval {
+		require Sub::Name;
+		no warnings 'redefine';
+		*subname = \&Sub::Name::subname;
+	};
+
+	*subname = sub { $_[1] } unless defined &subname;
+}
+
+# guess the fully qualified name for a sub using caller()
+sub full_name ($) {
+	my $name = shift;
+
+	croak "A subroutine name is required"
+		unless defined($name) and length($name);
+
+	return $name if $name =~ /::/;
+
+	foreach my $level ( 0 .. 2 ) {
+		my $pkg = caller($level);
+		next if $pkg eq __PACKAGE__;
+		return join "::", $pkg, $name;
+	}
+}
+
+
+our ( %variants, %default );
+
+sub multi ($$$) {
+	my ( $name, $case, $body ) = @_;
+
+	$name = full_name($name);
+
+
+	unless ( ref($body) and reftype($body) eq 'CODE' ) {
+		my $body_str = defined($body)
+			? ( ref($body) ? $body : "'$body'" )
+			: "undef";
+
+		croak "$body_str is not a code reference";
+	}
+
+	def_multi($name);
+
+	push @{ $variants{$name} }, [ $case, $body ];
+}
+
+sub multi_default ($$) {
+	my ( $name, $body ) = @_;
+
+	croak "$body is not a code reference"
+		unless ref($body) and reftype($body) eq 'CODE';
+
+	$name = full_name($name);
+
+	def_multi($name);
+
+	$default{$name} = $body;
+}
+
+sub def_multi ($;@) {
+	my ( $name, @args ) = @_;
+	$name = full_name($name);
+
+	unless ( exists $variants{$name} ) {
+		my @variants;
+
+		my $sub = sub {
+			given ( \@_ ) {
+				foreach my $variant ( @variants ) {
+					my ( $case, $body ) = @$variant;
+					when ( $case ) { goto $body }
+				}
+
+				default {
+					if ( my $default = $default{$name} ) {
+						goto $default;
+					} else {
+						croak "No variant found for arguments";
+					}
+				}
+			}
+		};
+
+		{
+			no strict 'refs';
+			*$name = subname $name, $sub;
+		}
+
+		$variants{$name} = \@variants;
+	}
+
+	def_variants($name, @args) if @args;
+}
+
+sub def_variants ($;) {
+	my ( $name, @variants ) = @_;
+
+	$name = full_name($name);
+
+	def_multi($name);
+
+	croak "The variant list is not even sized"
+		unless @variants % 2 == 0;
+
+	while ( @variants ) {
+	   	my ( $case, $body ) = splice(@variants, 0, 2);
+
+		if ( not ref($case) and $case ~~ 'default' ) {
+			multi_default $name, $body;
+		} else{
+			multi $name, $case, $body;
+		}
+	}
+}
+
+__PACKAGE__
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Sub::SmartMatch - Use smart matching to define multi subs
+
+=head1 SYNOPSIS
+
+	use Sub::SmartMatch;
+
+	use SmartMatch::Sugar qw(any);
+
+	# variants will be tried in a given/when
+	# clause in the order they are defined
+
+	multi fact => [ 0 ], sub { 1 };
+
+	multi fact => any, sub {
+		my $n = shift;
+		return $n * fact($n - 1);
+	}
+
+=head1 DESCRIPTION
+
+This module provides Haskell/ML style subroutine variants based on Perl's
+smartmatch operator.
+
+This doesn't do argument binding, just value matching.
+
+=head1 EXPORTS
+
+=over 4
+
+=item multi $name, $case, &body
+
+Define a variant for the sub name C<$name>.
+
+C<$case> will be smartmatched against an array reference of the arguments to
+the subroutine.
+
+=item multi_default $name, &body
+
+Define the C<default> for a multi sub. This variant is always tried last if no
+other variant matched.
+
+=item def_multi $name, [ $case => &body, $case => &body, default => ... ]
+
+Define a multi sub in one go.
+
+	def_multi foo => (
+		$case   => $body,
+		...     => ...,
+		default => $default,
+	);
+
+Is pretty much
+
+	use 5.010;
+
+	sub foo {
+		given ( \@_ ) {
+			when ( $case ) { goto $body }
+			when ( ... )   { goto ... }
+			default { goto $default }
+		}
+	}
+
+=back
+
+=head1 SEE ALSO
+
+L<Sub::PatternMatch>, L<perlsyn>, L<Class::Multimethods::Pure>
+
+=head1 VERSION CONTROL
+
+This module is maintained using Darcs. You can get the latest version from
+L<http://nothingmuch.woobling.org/code>, and use C<darcs send> to commit
+changes.
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
+
+=head1 COPYRIGHT
+
+	Copyright (c) 2008 Yuval Kogman. All rights reserved
+	This program is free software; you can redistribute
+	it and/or modify it under the same terms as Perl itself.
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More 'no_plan';
+
+use ok 'Sub::SmartMatch';
+
+multi "fact", [ 0 ], sub { 1 };
+
+multi "fact", sub () { 1 }, sub {
+	my $n = shift;
+	return $n * fact($n - 1);
+};
+
+is(fact(0), 1, "factorial of 0");
+is(fact(1), 1, "factorial of 1");
+is(fact(2), 2, "factorial of 2");
+is(fact(3), 6, "factorial of 6");
+
+
+def_multi other_fact => (
+	[0] => sub { 1 },
+	default => sub {
+		my $n = shift;
+		return $n * other_fact($n-1);
+	},
+);
+
+is( other_fact(5), 120, "multi_default" );
+
